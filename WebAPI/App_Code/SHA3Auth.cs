1bee3458ab7aa06f5b898e1d506945a594cdea6d
Scott R. Frost <ScottRFrost@gmail.com>
2013-10-25 15:21:42 -0400

    Simplify

--- WebAPI/App_Code/SHA3Auth.cs
+++ WebAPI/App_Code/SHA3Auth.cs
@@ -9,21 +9,21 @@ namespace WebAPI
     {
         public override void OnAuthorization(System.Web.Http.Controllers.HttpActionContext actionContext)
         {
-            System.Net.Http.HttpRequestMessage request = actionContext.Request;
+            var request = actionContext.Request;
 
             //Look for Authorization header
             if (!request.Headers.Contains("Authorization"))
             {
-                actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Authorization Header Required.");
+                actionContext.Response = request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Authorization Header Required.");
                 return;
             }
             
             //Split Username|UTC Date|Hash of url + localStorage.apikey + utcNumber
-            var auth = actionContext.Request.Headers.GetValues("Authorization");
+            var auth = request.Headers.GetValues("Authorization");
             var parts = auth.First().Split('|');
             if (parts.Length != 3)
             {
-                actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Authorization Header Malformed.  It should be three pipe separated parts.");
+                actionContext.Response = request.CreateErrorResponse(HttpStatusCode.BadRequest, "Authorization Header Malformed.  It should be three pipe separated parts.");
                 return;
             }
 
@@ -31,25 +31,25 @@ namespace WebAPI
             double utcTicks;
             if (!Double.TryParse(parts[1], out utcTicks))
             {
-                actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Time not numeric.  Send as ticks since 1/1/1970.");
+                actionContext.Response = request.CreateErrorResponse(HttpStatusCode.BadRequest, "Time not numeric.  Send as ticks since 1/1/1970.");
                 return;
             }
             var utcdate = new DateTime(1970, 1, 1).AddMilliseconds(utcTicks);
             var timeskew = utcdate.Subtract(DateTime.UtcNow);
             if (timeskew.TotalMinutes > 15 || timeskew.TotalMinutes < -15)
             {
-                actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Time too far from server time");
+                actionContext.Response = request.CreateErrorResponse(HttpStatusCode.BadRequest, "Time too far from server time");
                 return;
             }
 
             //Check vs API key in database
-            var url = actionContext.Request.RequestUri.AbsolutePath;
+            var url = request.RequestUri.AbsolutePath;
             var utf8 = new System.Text.UTF8Encoding();
             var sha3 = new SHA3Managed(512);
             var hashed = sha3.ComputeHash(utf8.GetBytes(url + "test" + parts[1])); //Load the middle portion from database, after looking up the API Key by the username
             if (parts[2] != Convert.ToBase64String(hashed))
             {
-                actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.Forbidden, "Bad API Key");
+                actionContext.Response = request.CreateErrorResponse(HttpStatusCode.Forbidden, "Bad API Key");
             }
         }
     }
